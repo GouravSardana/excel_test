a0e2822d0532bddd8103a92fb97b555d7882cca9
Gourav Sardana <gsardana001@gmail.com>
2018-12-07 11:47:10 +0530

    update

--- home/models.py
+++ home/models.py
@@ -90,6 +90,44 @@ class TemporaryBudgetApproval(models.Model):
     def __str__(self):
         return self.name
 
+class Temporary_Reimbursement_form(models.Model):
+    date=models.CharField(max_length=50)
+    item=models.CharField(max_length=50)
+    paid= models.CharField(max_length=50)
+    payment = models.CharField(max_length=50)
+    detail = models.CharField(max_length=50)
+    amount = models.CharField(max_length=50)
+    remark= models.CharField(max_length=50)
+    subtotal = models.CharField(max_length=50)
+    total = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Temporary_Purchase_order(models.Model):
+    name = models.ForeignKey(Person_name, on_delete=models.CASCADE)
+    purchase_no=models.CharField(max_length=50)
+    date_of_purchase=models.CharField(max_length=50)
+    attension = models.CharField(max_length=50)
+    contact_person = models.CharField(max_length=50)
+    tel = models.CharField(max_length=50)
+    ventor= models.CharField(max_length=50)
+    delivery_date= models.CharField(max_length=50)
+    delivery_purchase = models.CharField(max_length=50)
+    no = models.CharField(max_length=50)
+    product= models.CharField(max_length=50)
+    spec= models.CharField(max_length=50)
+    unit= models.CharField(max_length=50)
+    quantity = models.CharField(max_length=50)
+    unit_price = models.CharField(max_length=50)
+    sum = models.CharField(max_length=50)
+    remark = models.CharField(max_length=50)
+    total = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 
 
 
--- home/views.py
+++ home/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 
-from home.models import Budget_Approval, TemporaryBudgetApproval
+from home.models import Budget_Approval, TemporaryBudgetApproval, Temporary_Reimbursement_form, \
+    Temporary_Purchase_order, Reimbursement_form
 
 
 class Demo(TemplateView):
@@ -13,30 +14,51 @@ class Demo(TemplateView):
     def post(self, request, *args, **kwargs):
         c = self.request.POST.get('index')
         name=self.request.POST.get('name')
+        form_name = self.request.POST.get('form-name')
         TemporaryBudgetApproval.objects.all().delete()
+        Temporary_Reimbursement_form.objects.all().delete()
+        Temporary_Purchase_order.objects.all().delete()
         if c==None or c== '':
             c=1
         else:
             c = int(self.request.POST.get('index'))
         for i in range(1, c + 1):
-            purpose = self.request.POST.get('txtpurpose'+ str(i))
-            request_date = self.request.POST.get('txtreqdate'+ str(i))
-            # request_approval_date = self.request.POST.get('txtreqappdate'+ str(i))
-            delivery_due_date = self.request.POST.get('txtdelivplace'+ str(i))
-            number = self.request.POST.get('txtnol'+ str(i))
-            ventor = self.request.POST.get('txtvendor'+ str(i))
-            product = self.request.POST.get('txtproductt'+ str(i))
-            spec = self.request.POST.get('txtspecc'+ str(i))
-            quantity = self.request.POST.get('txtquant'+ str(i))
-            unit_price = self.request.POST.get('txtunitpricee'+ str(i))
-            total_price = self.request.POST.get('txttotalprice'+ str(i))
-            remark = self.request.POST.get('txtremarkk'+ str(i))
-            total = self.request.POST.get('txttotalmoney'+ str(i))
-            form=Budget_Approval(name=name, purpose=purpose, request_date=request_date, delivery_due_date=delivery_due_date, number=number, ventor=ventor, product=product, spec=spec, quantity=quantity,unit_price=unit_price, total_price=total_price, remark=remark, total=total)
-            form1=TemporaryBudgetApproval(name=name, purpose=purpose, request_date=request_date, delivery_due_date=delivery_due_date, number=number, ventor=ventor, product=product, spec=spec, quantity=quantity,unit_price=unit_price, total_price=total_price, remark=remark, total=total)
-            form.save()
-            form1.save()
-            print(TemporaryBudgetApproval)
+                purpose = self.request.POST.get('txtpurpose'+ str(i))
+                request_date = self.request.POST.get('txtreqdate'+ str(i))
+                # request_approval_date = self.request.POST.get('txtreqappdate'+ str(i))
+                delivery_due_date = self.request.POST.get('txtdelivplace'+ str(i))
+                number = self.request.POST.get('txtnol'+ str(i))
+                ventor = self.request.POST.get('txtvendor'+ str(i))
+                product = self.request.POST.get('txtproductt'+ str(i))
+                spec = self.request.POST.get('txtspecc'+ str(i))
+                quantity = self.request.POST.get('txtquant'+ str(i))
+                unit_price = self.request.POST.get('txtunitpricee'+ str(i))
+                total_price = self.request.POST.get('txttotalprice'+ str(i))
+                remark = self.request.POST.get('txtremarkk'+ str(i))
+                total = self.request.POST.get('txttotalmoney'+ str(i))
+                form=Budget_Approval(name=name, purpose=purpose, request_date=request_date, delivery_due_date=delivery_due_date, number=number, ventor=ventor, product=product, spec=spec, quantity=quantity,unit_price=unit_price, total_price=total_price, remark=remark, total=total)
+                form1=TemporaryBudgetApproval(name=name, purpose=purpose, request_date=request_date, delivery_due_date=delivery_due_date, number=number, ventor=ventor, product=product, spec=spec, quantity=quantity,unit_price=unit_price, total_price=total_price, remark=remark, total=total)
+                form.save()
+                form1.save()
+                print(TemporaryBudgetApproval)
+            # elif form_name=='Reimbursement Form':
+            #     date = self.request.POST.get('txtdate1' + str(i))
+            #     item = self.request.POST.get('txtitems1' + str(i))
+            #     paid = self.request.POST.get('txtpaidto1'+ str(i))
+            #     payment = self.request.POST.get('txtpaymethod1' + str(i))
+            #     detail = self.request.POST.get('txtdetails1' + str(i))
+            #     amount = self.request.POST.get('txtamount1' + str(i))
+            #     remark = self.request.POST.get('txtremarks1' + str(i))
+            #     subtotal = self.request.POST.get('txtsubtotal1' + str(i))
+            #     total = self.request.POST.get('txttotal1' + str(i))
+            #     form=Reimbursement_form(date=date, item=item, paid=paid, payment=payment, detail=detail, amount=amount, remark=remark, subtotal=subtotal, total=total)
+            #     form1=Temporary_Reimbursement_form(date=date, item=item, paid=paid, payment=payment, detail=detail, amount=amount, remark=remark, subtotal=subtotal, total=total)
+            #     print(form)
+            #     form.save()
+            #     form1.save()
+            # else:
+            #     return HttpResponse('lol')
+
         return HttpResponse('Done')
 
 
@@ -57,3 +79,16 @@ class Temp_excel(ListView):
         budget = TemporaryBudgetApproval.objects.all()
         print(budget)
         return render(request, 'temp_excel.html', {'f': budget})
+
+
+
+
+
+
+class Template_re_form(ListView):
+    template_name = 'template_re_form'
+
+    def get(self, request, *args, **kwargs):
+        budget = Temporary_Reimbursement_form.objects.all()
+        print(budget)
+        return render(request, 'template_re_form.html', {'f': budget})
